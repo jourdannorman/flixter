7f53e6c8be963edbd465e53815c42bd7133adb78
Jourdan Norman <jourdan.norman@icloud.com>
2017-02-09 21:56:36 +0000

    Course validation

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,7 @@
 class Course < ApplicationRecord
   belongs_to :user
+
+  validates :title, :presence => { :message => "Course title is required" }
+  validates :description, :presence => { :message => "Description is required" }
+  validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
 end
