d53c01265569c728f9aa1d9a09d261d7b876503c
Jourdan Norman <jourdan.norman@icloud.com>
2017-02-22 22:51:42 +0000

    Lesson permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,19 @@
 class LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :require_course_enrollment_for_lessons, only: [:show]
+
 	def show
 	end
 
 	private
+
+	def require_course_enrollment_for_lessons
+		this_course = current_lesson.section.course
+
+		if !current_user.enrolled_in?(this_course)
+			redirect_to course_path(this_course), alert: 'Please enroll to access lessons'
+		end
+	end
 
 	helper_method :current_lesson
 	def current_lesson
